58b94adfdcb9b58937c871c6ea86bbe96cef40f7
daivan trinh <daivan.trinh@aktivbo.se>
2022-01-10 17:58:17 +0100

    Added save and load to the background.js

--- background.js
+++ background.js
@@ -13,3 +13,14 @@ chrome.runtime.onInstalled.addListener((reason) => {
     });
   //}
 });
+
+
+   // Save it using the Chrome extension storage API.
+   chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
+    console.log('Settings saved');
+  });
+
+  // Read it using the storage API
+  chrome.storage.sync.get(['foo', 'bar'], function(items) {
+    console.log('Settings retrieved', items);
+  });
