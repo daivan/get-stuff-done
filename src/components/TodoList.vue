e479db4a552a7652ed21ade8181c46ee34e369cb
daivan trinh <daivan.trinh@aktivbo.se>
2022-01-13 22:32:35 +0100

    Have save and load

---
+++ src/components/TodoList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    <h1 v-on:click="hej">{{ msg }} CLICK HERE</h1>
+{{posts}}
+  <TodoListItem
+    v-for="post in posts"
+    :key="post.id"
+    :title="post.title"
+  ></TodoListItem>
+
+  </div>
+</template>
+
+<script>
+import TodoListItem from './TodoListItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoListItem
+  },
+    props: {
+    msg: String
+  },
+data() {
+  return {
+    foo: 1,
+    posts: [ 
+      {id: 1, title: 'first'},
+      {id: 2, title: 'second'}
+       ]
+  }
+},
+  methods:{
+    hej(){
+      console.log('is this how its done?')
+      let key = 'foo'
+      localStorage.setItem(key, 'we doin it!');
+      let bajs = localStorage.getItem(key);
+      console.log('load', bajs)
+         
+
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todoListItem">
+    <label><input type="checkbox" /> {{title}}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    id: String,
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
